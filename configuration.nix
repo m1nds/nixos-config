f9958b3aafc80f1c31c4f37248f7c03bcb76532c
Lyes BOURENNANI <lyes.bourennani@epita.fr>
2024-08-27 22:35:09 +0200

    fix: fix bluetooth using pulseaudio

--- configuration.nix
+++ configuration.nix
@@ -28,6 +28,10 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  hardware.bluetooth.enable = true;
+  hardware.pulseaudio.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
   # Set your time zone.
   time.timeZone = "Europe/Paris";
 
@@ -82,25 +86,25 @@
       enable = true;
     };
 
-    pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      wireplumber.extraConfig."10-bluez" = {
-    	"monitor.bluez.properties" = {
-      	  "bluez5.enable-sbc-xq" = true;
-      	  "bluez5.enable-msbc" = true; 
-      	  "bluez5.enable-hw-volume" = true;
-      	  "bluez5.headset-roles" = [
-            "hsp_hs"
-            "hsp_ag"
-            "hfp_hf"
-            "hfp_ag"
-          ];
-        };
-      };
-    };
+    #pipewire = {
+      #enable = true;
+      #alsa.enable = true;
+      #alsa.support32Bit = true;
+      #pulse.enable = true;
+      #wireplumber.extraConfig."10-bluez" = {
+    	#"monitor.bluez.properties" = {
+      	  #"bluez5.enable-sbc-xq" = true;
+      	  #"bluez5.enable-msbc" = true; 
+      	  #"bluez5.enable-hw-volume" = true;
+      	  #"bluez5.headset-roles" = [
+            #"hsp_hs"
+            #"hsp_ag"
+            #"hfp_hf"
+            #"hfp_ag"
+          #];
+        #};
+      #};
+    #};
   };
 
   #services.xserver.xkb = {
